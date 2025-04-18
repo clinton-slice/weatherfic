e695ac5146b286586e292d41cb202b257abbbd84
clinton-slice <ccnkemdilim@gmail.com>
2025-04-18 10:07:41 -0400

    Update project configuration and dependencies
    - Added @types/node as a development dependency in package.json and package-lock.json.
    - Updated tsconfig.app.json to include a new path alias for contexts.
    - Enhanced vite.config.ts with additional path aliases for better module resolution.

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,21 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import tailwindcss from "@tailwindcss/vite";
+import * as path from "path";
 
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  resolve: {
+    alias: {
+      "@": "/src",
+      "@public": path.resolve(__dirname, "public"),
+      "@assets": path.resolve(__dirname, "src/assets"),
+      "@components": path.resolve(__dirname, "src/components"),
+      "@contexts": path.resolve(__dirname, "src/contexts"),
+      "@pages": path.resolve(__dirname, "src/pages"),
+      "@styles": path.resolve(__dirname, "src/styles"),
+      "@utils": path.resolve(__dirname, "src/utils"),
+    },
+  },
 });
