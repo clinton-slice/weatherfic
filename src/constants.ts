dae873a22dda98933c9f7d9b49ae5b2b02ce3e2a
clinton-slice <ccnkemdilim@gmail.com>
2025-04-18 18:17:12 -0400

    Add city constants for Vancouver and Ottawa

---
+++ src/constants.ts
@@ -0,0 +1,12 @@
+// Default location (Vancouver, BC, Canada)
+export const VANCOUVER_CITY = {
+  name: "Vancouver",
+  lat: 49.2827,
+  lon: -123.1207,
+};
+
+export const OTTAWA_CITY = {
+  name: "Ottawa",
+  lat: 45.4215,
+  lon: -75.6972,
+};
